524e1decbbecaf63ab35f97316c31c327b329afd
mpd97 <mateuszpracadom@gmail.com>
2020-03-19 20:01:52 +0100

    New Testing project

---
+++ HighPerformanceSharp/StringOperations/ITestString.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StringOperations
+{
+    partial interface ITestString
+    {
+        public void Initialize();
+        public void Test();
+        public void Validate();
+    }
+}
---
+++ HighPerformanceSharp/StringOperations/StringTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StringOperations
+{
+    public class StringTest : ITestString
+    {
+        public void Initialize()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Test()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Validate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
